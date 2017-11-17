bfe0871dc03eec35cdf9aa3ee8f6d2735b6e6183
Codadillo <leoconr@nuevaschool.org>
2017-11-17 15:39:23 -0800

    Fixed error where dimensions were accidentally reversed.

--- lib/src/read_excel.dart
+++ lib/src/read_excel.dart
@@ -13,7 +13,20 @@ String readCell(final z, final x, final path) { //the spreadsheet is oriented so
   final decoder = new SpreadsheetDecoder.decodeBytes(bytes); //decode the bytes into readable spreadsheet
   final table = decoder.tables['Sheet1']; //get 2d array of first sheet
   final values = table.rows[x]; //get values of desired row
-  return values[z]; //get desired value from row
+  print("length of values: " + values.length.toString());
+  print("requested z:" + z.toString());
+  return values[z].toString(); //get desired value from row
+}
+
+List<int> getDimensions(path) {
+  final newUri = Uri.parse(path); //get Uri from file path
+  final bytes = new File.fromUri(newUri).readAsBytesSync(); //read the file into bytes
+  final decoder = new SpreadsheetDecoder.decodeBytes(bytes); //decode the bytes into readable spreadsheet
+  final table = decoder.tables['Sheet1']; //get 2d array of first sheet
+  final rows = table.rows;
+  int height = rows.length;
+  int width = rows[0].length;
+  return [width, height];
 }
 
 void finalCommand(String path, int cornerX, int cornerY, int cornerZ) {
@@ -23,19 +36,22 @@ void finalCommand(String path, int cornerX, int cornerY, int cornerZ) {
   List<int> dirCons = [];
   List<String> types = [];
   List<bool> actives = [];
-
-  for (int z = 0; z < 1; ++z) {
-    for (int x = 0; x < 2; ++x) {
+  print(getDimensions(path)[0].toString() + ", " + getDimensions(path)[1].toString());
+  for (int z = 0; z < getDimensions(path)[0]; ++z) {
+    for (int x = 0; x < getDimensions(path)[1]; ++x) {
       var content = readCell(z, x, path);
-      commands.add(decryptCBlockCom(content));
-      dirCons.add(decryptCBlockDirCon(content));
-      types.add(decryptCBlockType(content));
-      actives.add(decryptCBlockActive(content));
-      zs.add(z);
-      xs.add(x);
+      if (content.contains(';')) {
+        commands.add(decryptCBlockCom(content));
+        dirCons.add(decryptCBlockDirCon(content));
+        types.add(decryptCBlockType(content));
+        actives.add(decryptCBlockActive(content));
+        zs.add(z);
+        xs.add(x);
+      }
     }
   }
   print(commandFromValues(xs, [0, 0], zs, commands, dirCons, actives, types, cornerX, cornerY, cornerZ));
 }
+
 
 main() => finalCommand("/Users/leoconr/Downloads/Workbook1.xlsx", 2, 4, 4);
