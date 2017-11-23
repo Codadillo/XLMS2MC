2187e6118b3e843517ddd0d42a91afedb2fe34af
Codadillo <leoconr@nuevaschool.org>
2017-11-22 18:58:10 -0800

    SOS

--- web/main.dart
+++ web/main.dart
@@ -3,7 +3,6 @@ import 'dart:html';
 import 'dart:typed_data';
 import 'package:xlsx2mc/xlsx2mc.dart';
 
-
 void main() {
   final form = querySelector('#spreadsheet-form');
   form.onSubmit.listen(onFormSubmit);
@@ -13,22 +12,23 @@ Future<Null> onFormSubmit(Event event) async {
   final input = querySelector('#file-input') as FileUploadInputElement;
   final output = querySelector('#output');
   List<File> files = input.files;
-
+  event.preventDefault();
   if (files.isEmpty) {
     output.text = "Woah there budy-o, you got'tsa input a fily-o";
-    event.preventDefault();
+//    event.preventDefault();
   } else if (input.value.split(".")[1] != "xlsx") {
     output.text = "You must submit a file of the type '.xlsx'.";
-    event.preventDefault();
+//    event.preventDefault();
   } else {
+//    print((await readFile(files.first)).runtimeType);
     Uint8List bytes = await readFile(files.first);
     output.text = new spreadsheetInfo(bytes).finalCommand(0, 0, 0);
-    event.preventDefault();
+
   }
 }
 
 Future<Uint8List> readFile(Blob blob) {
-  final completer = new Completer();
+  final completer = new Completer<Uint8List>();
   final reader = new FileReader();
 
   reader.onLoad.listen((_) => completer.complete(reader.result));
